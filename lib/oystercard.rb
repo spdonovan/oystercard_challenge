7c752af42a9ed3dab4f53bc338dcdce05f273070
ScottDonovan <spdonovan@hotmail.co.uk>
2021-10-18 16:45:04 +0100

    journey, touch_in, touch_out

--- lib/oystercard.rb
+++ lib/oystercard.rb
@@ -1,15 +1,29 @@
 class Oystercard
 attr_reader :balance
+attr_reader :in_journey
 CARD_LIMIT = 90
 
   def initialize
     @balance = 0
     @card_limit = CARD_LIMIT
+    @in_journey = false
   end
 
   def top_up(amount)
     raise "Card limit reached #{@card_limit}" if (@balance + amount > @card_limit)
     @balance += amount
+  end
+
+  def deduct(amount)
+    @balance -= amount
+  end
+
+  def touch_in
+    @in_journey = true
+  end
+
+  def touch_out
+    @in_journey = false
   end
 
 end
--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -11,9 +11,25 @@ initial_balance = subject.balance
 expect(subject.top_up(1)).to eq(initial_balance + 1)
 end
 
+it 'Can deduct from card' do
+initial_balance = subject.balance
+expect(subject.deduct(1)).to eq(initial_balance - 1)
+end
+
 it 'Raise error when card is at maximum amount of £90' do
   subject.top_up(Oystercard::CARD_LIMIT)
   expect{ subject.top_up(1) }.to raise_error("Card limit reached #{Oystercard::CARD_LIMIT}")
 end
 
+it 'responds to card in journey' do
+  expect(subject.in_journey).to eq(false)
+end
+
+it 'responds to touch in' do
+  expect(subject.touch_in).to eq(true)
+end
+
+it 'responds to touch out' do
+  expect(subject.touch_out).to eq(false)
+end
 end
