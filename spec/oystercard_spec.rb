fb665b5919a4090a03b15b82c4905fa1503a004e
Dean Smythe <dean@teamtwisted.co.uk>
2021-10-18 14:47:22 +0100

    top_up balance with credit

--- spec/oystercard_spec.rb
+++ spec/oystercard_spec.rb
@@ -6,4 +6,9 @@ describe Oystercard do
     expect(subject.balance).to eq(0)
   end
 
+it ' Can top up card' do
+initial_balance = subject.balance
+expect(subject.top_up(1)).to eq(initial_balance + 1)
+end
+
 end
